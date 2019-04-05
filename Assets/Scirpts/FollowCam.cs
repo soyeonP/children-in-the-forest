3e2b11eaf29096f20aebb523842d7abdbb57a5bc
soyeonP <uyyu173@naver.com>
2019-04-06 05:22:24 +0900

    Moving_beforeAnimation
    애니메이션수정전

---
+++ Assets/Scirpts/FollowCam.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCam : MonoBehaviour
+{ 
+    public Transform target; 
+    public Transform target1;
+    public Transform target2;
+    public Transform target3;
+    public Transform target4;
+
+    public const float offsetX = 0f;
+    public const float offsetY = 0f;
+    public const float offsetZ = -10f;
+    public float limitright = 2f;
+    //public float limitleft = -2f;
+    public float limittop = 2f;
+    //public float limitbottom = -2f;
+
+    public float followSpeed = 10.0f;
+
+    private Transform tr;
+    Vector3 cameraPosition; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        tr = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+  
+
+        cameraPosition = new Vector3 (target.position.x, target.position.y, offsetZ);
+
+        if (target.position.x > limitright)
+            cameraPosition.x = limitright;
+        else if (target.position.x < -(limitright))
+            cameraPosition.x = -(limitright);
+
+        if (target.position.y > limittop)
+            cameraPosition.y = limittop;
+        else if (target.position.y < -(limittop))
+            cameraPosition.y = -(limittop);
+
+        transform.position = Vector3.Lerp(tr.position, cameraPosition, followSpeed * Time.deltaTime);
+    }
+}
---
+++ Assets/Scirpts/playerState.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerState : MonoBehaviour
+{
+    public float stamina= 10.0f;
+    public float Hp = 10.0f ;
+    public float Scare =10.0f ;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //몬스터가 공격시 hp 감소
+        if(stamina > 0)
+        stamina -= (0.1f * Time.deltaTime);
+           //플레이어 사망 클래스만들고/ 클래스 소환. 
+        //음식아이템 섭취시 stamina 증가 
+
+        
+    }
+}
